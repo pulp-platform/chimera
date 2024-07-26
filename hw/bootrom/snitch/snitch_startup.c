d01b509303f166f70b3185b054d8f345201d9b31
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2024-07-26 18:10:45 +0200

    Fix formatting

--- hw/bootrom/snitch/snitch_startup.c
+++ hw/bootrom/snitch/snitch_startup.c
@@ -1,8 +1,8 @@
 /* =====================================================================
  * Title:        snitch_startup.c
- * Description:  
+ * Description:
  *
- * $Date:        28.06.2024        
+ * $Date:        28.06.2024
  *
  * ===================================================================== */
 /*
@@ -29,49 +29,57 @@
 #include <regs/soc_ctrl.h>
 #include <soc_addr_map.h>
 
-#define set_csr(reg, bit) ({ unsigned long __tmp; \
-  if (__builtin_constant_p(bit) && (unsigned long)(bit) < 32) \
-    asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
-  else \
-    asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
-  __tmp; })
+#define set_csr(reg, bit) \
+    ({ \
+        unsigned long __tmp; \
+        if (__builtin_constant_p(bit) && (unsigned long)(bit) < 32) \
+            asm volatile("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
+        else \
+            asm volatile("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
+        __tmp; \
+    })
 
-#define IRQ_M_SOFT    3
+#define IRQ_M_SOFT 3
 
-#define MSTATUS_MIE         0x00000008
-#define MIP_MSIP            (1 << IRQ_M_SOFT)
+#define MSTATUS_MIE 0x00000008
+#define MIP_MSIP (1 << IRQ_M_SOFT)
 
-void cluster_startup(){
-  set_csr(mie, MIP_MSIP);
-  set_csr(mstatus, MSTATUS_MIE);  // set M global interrupt enable
-  return;
+void cluster_startup() {
+    set_csr(mie, MIP_MSIP);
+    set_csr(mstatus, MSTATUS_MIE); // set M global interrupt enable
+    return;
 }
 
-void cluster_return(uint32_t ret){
+void cluster_return(uint32_t ret) {
 
-  uint32_t retVal = ret | 0x000000001;
-  
-  uint8_t hartId;
-  asm ("csrr %0, mhartid" : "=r" (hartId) ::);
+    uint32_t retVal = ret | 0x000000001;
 
-  switch(hartId){
+    uint8_t hartId;
+    asm("csrr %0, mhartid" : "=r"(hartId)::);
 
-  case 1:
-    *((volatile uint32_t*)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_1_RETURN_REG_OFFSET)) = retVal;
-    break;
-  case 10:
-    *((volatile uint32_t*)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_2_RETURN_REG_OFFSET)) = retVal;
-    break;
-  case 19:
-    *((volatile uint32_t*)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_3_RETURN_REG_OFFSET)) = retVal;
-    break;
-  case 28:
-    *((volatile uint32_t*)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_4_RETURN_REG_OFFSET)) = retVal;
-    break;
-  case 37:
-    *((volatile uint32_t*)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_5_RETURN_REG_OFFSET)) = retVal;
-    break;
-  }
+    switch (hartId) {
 
-  return;
+    case 1:
+        *((volatile uint32_t *)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_1_RETURN_REG_OFFSET)) =
+            retVal;
+        break;
+    case 10:
+        *((volatile uint32_t *)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_2_RETURN_REG_OFFSET)) =
+            retVal;
+        break;
+    case 19:
+        *((volatile uint32_t *)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_3_RETURN_REG_OFFSET)) =
+            retVal;
+        break;
+    case 28:
+        *((volatile uint32_t *)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_4_RETURN_REG_OFFSET)) =
+            retVal;
+        break;
+    case 37:
+        *((volatile uint32_t *)(SOC_CTRL_BASE + CHIMERA_SNITCH_CLUSTER_5_RETURN_REG_OFFSET)) =
+            retVal;
+        break;
+    }
+
+    return;
 }
