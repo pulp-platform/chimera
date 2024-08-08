4f77244e50c98091e939e253decd62da83580143
Lorenzo Leone <lleone@iis.ee.ethz.ch>
2024-08-08 10:49:38 +0200

    Adapt lint-license

--- target/asic/src/padSocInterconnect/pad_soc_intf.sv
+++ target/asic/src/padSocInterconnect/pad_soc_intf.sv
@@ -1,24 +1,8 @@
-// ----------------------------------------------------------------------
-//
-// File: pad_soc_intf.sv
-//
-// Created: 24.07.2024
-//
-// Copyright (C) 2024, ETH Zurich and University of Bologna.
-//
-// Author: Lorenzo Leone, ETH Zurich
-//
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 //
-// Copyright and related rights are licensed under the Solderpad Hardware License,
-// Version 0.51 (the "License"); you may not use this file except in compliance with
-// the License. You may obtain a copy of the License at http://solderpad.org/licenses/SHL-0.51.
-// Unless required by applicable law or agreed to in writing, software, hardware and materials
-// distributed under this License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and limitations under the License.
-//
-// ----------------------------------------------------------------------
+// Lorenzo Leone <lleone@iis.ee.ethz.ch>
 
 `define map_gpio(_group,_indx) \
 assign port_signals_soc2pad_o.aon_gpio``_group.gpio``_group``_indx.gpio``_o = gpio_i``[``_indx``]; \
