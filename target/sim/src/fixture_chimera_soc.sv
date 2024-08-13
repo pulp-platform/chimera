c6ad00ef65c446df000ed3ec709bdc7e753f2616
MoritzScherer <scheremo@iis.ee.ethz.ch>
2024-08-13 15:07:45 +0200

    Add support for 32 AXI Data & Address width (#23)
    * HW: Add support for 32 bit SoC config

--- target/sim/src/fixture_chimera_soc.sv
+++ target/sim/src/fixture_chimera_soc.sv
@@ -27,90 +27,76 @@ module fixture_chimera_soc #(
   //  DUT  //
   ///////////
 
-  logic                                       soc_clk;
-  logic                                       clu_clk;
-  logic                                       rst_n;
-  logic                                       test_mode;
-  logic [             1:0]                    boot_mode;
-  logic                                       rtc;
+  logic                 soc_clk;
+  logic                 clu_clk;
+  logic                 rst_n;
+  logic                 test_mode;
+  logic [          1:0] boot_mode;
+  logic                 rtc;
 
-  logic                                       jtag_tck;
-  logic                                       jtag_trst_n;
-  logic                                       jtag_tms;
-  logic                                       jtag_tdi;
-  logic                                       jtag_tdo;
+  logic                 jtag_tck;
+  logic                 jtag_trst_n;
+  logic                 jtag_tms;
+  logic                 jtag_tdi;
+  logic                 jtag_tdo;
 
-  logic                                       uart_tx;
-  logic                                       uart_rx;
+  logic                 uart_tx;
+  logic                 uart_rx;
 
-  logic                                       i2c_sda_o;
-  logic                                       i2c_sda_i;
-  logic                                       i2c_sda_en;
-  logic                                       i2c_scl_o;
-  logic                                       i2c_scl_i;
-  logic                                       i2c_scl_en;
+  logic                 i2c_sda_o;
+  logic                 i2c_sda_i;
+  logic                 i2c_sda_en;
+  logic                 i2c_scl_o;
+  logic                 i2c_scl_i;
+  logic                 i2c_scl_en;
 
-  logic                                       spih_sck_o;
-  logic                                       spih_sck_en;
-  logic [   SpihNumCs-1:0]                    spih_csb_o;
-  logic [   SpihNumCs-1:0]                    spih_csb_en;
-  logic [             3:0]                    spih_sd_o;
-  logic [             3:0]                    spih_sd_i;
-  logic [             3:0]                    spih_sd_en;
-
-  logic [SlinkNumChan-1:0]                    slink_rcv_clk_i;
-  logic [SlinkNumChan-1:0]                    slink_rcv_clk_o;
-  logic [SlinkNumChan-1:0][SlinkNumLanes-1:0] slink_i;
-  logic [SlinkNumChan-1:0][SlinkNumLanes-1:0] slink_o;
+  logic                 spih_sck_o;
+  logic                 spih_sck_en;
+  logic [SpihNumCs-1:0] spih_csb_o;
+  logic [SpihNumCs-1:0] spih_csb_en;
+  logic [          3:0] spih_sd_o;
+  logic [          3:0] spih_sd_i;
+  logic [          3:0] spih_sd_en;
 
   chimera_top_wrapper #(
     .SelectedCfg(SelectedCfg)
   ) dut (
-    .soc_clk_i      (soc_clk),
-    .clu_clk_i      (clu_clk),
-    .rst_ni         (rst_n),
-    .test_mode_i    (test_mode),
-    .boot_mode_i    (boot_mode),
-    .rtc_i          (rtc),
-    .jtag_tck_i     (jtag_tck),
-    .jtag_trst_ni   (jtag_trst_n),
-    .jtag_tms_i     (jtag_tms),
-    .jtag_tdi_i     (jtag_tdi),
-    .jtag_tdo_o     (jtag_tdo),
-    .jtag_tdo_oe_o  (),
-    .uart_tx_o      (uart_tx),
-    .uart_rx_i      (uart_rx),
-    .uart_rts_no    (),
-    .uart_dtr_no    (),
-    .uart_cts_ni    (1'b0),
-    .uart_dsr_ni    (1'b0),
-    .uart_dcd_ni    (1'b0),
-    .uart_rin_ni    (1'b0),
-    .i2c_sda_o      (i2c_sda_o),
-    .i2c_sda_i      (i2c_sda_i),
-    .i2c_sda_en_o   (i2c_sda_en),
-    .i2c_scl_o      (i2c_scl_o),
-    .i2c_scl_i      (i2c_scl_i),
-    .i2c_scl_en_o   (i2c_scl_en),
-    .spih_sck_o     (spih_sck_o),
-    .spih_sck_en_o  (spih_sck_en),
-    .spih_csb_o     (spih_csb_o),
-    .spih_csb_en_o  (spih_csb_en),
-    .spih_sd_o      (spih_sd_o),
-    .spih_sd_en_o   (spih_sd_en),
-    .spih_sd_i      (spih_sd_i),
-    .gpio_i         ('0),
-    .gpio_o         (),
-    .gpio_en_o      (),
-    .slink_rcv_clk_i(slink_rcv_clk_i),
-    .slink_rcv_clk_o(slink_rcv_clk_o),
-    .slink_i        (slink_i),
-    .slink_o        (slink_o),
-    .vga_hsync_o    (),
-    .vga_vsync_o    (),
-    .vga_red_o      (),
-    .vga_green_o    (),
-    .vga_blue_o     ()
+    .soc_clk_i    (soc_clk),
+    .clu_clk_i    (clu_clk),
+    .rst_ni       (rst_n),
+    .test_mode_i  (test_mode),
+    .boot_mode_i  (boot_mode),
+    .rtc_i        (rtc),
+    .jtag_tck_i   (jtag_tck),
+    .jtag_trst_ni (jtag_trst_n),
+    .jtag_tms_i   (jtag_tms),
+    .jtag_tdi_i   (jtag_tdi),
+    .jtag_tdo_o   (jtag_tdo),
+    .jtag_tdo_oe_o(),
+    .uart_tx_o    (uart_tx),
+    .uart_rx_i    (uart_rx),
+    .uart_rts_no  (),
+    .uart_dtr_no  (),
+    .uart_cts_ni  (1'b0),
+    .uart_dsr_ni  (1'b0),
+    .uart_dcd_ni  (1'b0),
+    .uart_rin_ni  (1'b0),
+    .i2c_sda_o    (i2c_sda_o),
+    .i2c_sda_i    (i2c_sda_i),
+    .i2c_sda_en_o (i2c_sda_en),
+    .i2c_scl_o    (i2c_scl_o),
+    .i2c_scl_i    (i2c_scl_i),
+    .i2c_scl_en_o (i2c_scl_en),
+    .spih_sck_o   (spih_sck_o),
+    .spih_sck_en_o(spih_sck_en),
+    .spih_csb_o   (spih_csb_o),
+    .spih_csb_en_o(spih_csb_en),
+    .spih_sd_o    (spih_sd_o),
+    .spih_sd_en_o (spih_sd_en),
+    .spih_sd_i    (spih_sd_i),
+    .gpio_i       ('0),
+    .gpio_o       (),
+    .gpio_en_o    ()
   );
 
   ////////////////////////
@@ -129,11 +115,6 @@ module fixture_chimera_soc #(
   ///////////
   //  VIP  //
   ///////////
-
-  axi_mst_req_t axi_slink_mst_req;
-  axi_mst_rsp_t axi_slink_mst_rsp;
-
-  assign axi_slink_mst_req = '0;
 
   vip_chimera_soc #(
     .DutCfg           (DutCfg),
--- target/sim/src/vip_chimera_soc.sv
+++ target/sim/src/vip_chimera_soc.sv
@@ -42,35 +42,28 @@ module vip_chimera_soc
   parameter int unsigned AxiStrbWidth      = DutCfg.AxiDataWidth / 8,
   parameter int unsigned AxiStrbBits       = $clog2(DutCfg.AxiDataWidth / 8)
 ) (
-  output logic                                                   soc_clk,
-  output logic                                                   clu_clk,
-  output logic                                                   rst_n,
-  output logic                                                   test_mode,
-  output logic             [             1:0]                    boot_mode,
-  output logic                                                   rtc,
-  input  axi_ext_mst_req_t                                       axi_slink_mst_req,
-  output axi_ext_mst_rsp_t                                       axi_slink_mst_rsp,
+  output logic                 soc_clk,
+  output logic                 clu_clk,
+  output logic                 rst_n,
+  output logic                 test_mode,
+  output logic [          1:0] boot_mode,
+  output logic                 rtc,
   // JTAG interface
-  output logic                                                   jtag_tck,
-  output logic                                                   jtag_trst_n,
-  output logic                                                   jtag_tms,
-  output logic                                                   jtag_tdi,
-  input  logic                                                   jtag_tdo,
+  output logic                 jtag_tck,
+  output logic                 jtag_trst_n,
+  output logic                 jtag_tms,
+  output logic                 jtag_tdi,
+  input  logic                 jtag_tdo,
   // UART interface
-  input  logic                                                   uart_tx,
-  output logic                                                   uart_rx,
+  input  logic                 uart_tx,
+  output logic                 uart_rx,
   // I2C interface
-  inout  wire                                                    i2c_sda,
-  inout  wire                                                    i2c_scl,
+  inout  wire                  i2c_sda,
+  inout  wire                  i2c_scl,
   // SPI host interface
-  inout  wire                                                    spih_sck,
-  inout  wire              [   SpihNumCs-1:0]                    spih_csb,
-  inout  wire              [             3:0]                    spih_sd,
-  // Serial link interface
-  output logic             [SlinkNumChan-1:0]                    slink_rcv_clk_i,
-  input  logic             [SlinkNumChan-1:0]                    slink_rcv_clk_o,
-  output logic             [SlinkNumChan-1:0][SlinkNumLanes-1:0] slink_i,
-  input  logic             [SlinkNumChan-1:0][SlinkNumLanes-1:0] slink_o
+  inout  wire                  spih_sck,
+  inout  wire  [SpihNumCs-1:0] spih_csb,
+  inout  wire  [          3:0] spih_sd
 );
 
   `include "cheshire/typedef.svh"
@@ -150,7 +143,7 @@ module vip_chimera_soc
   localparam dm::sbcs_t JtagInitSbcs = '{
       sbautoincrement: 1'b1,
       sbreadondata: 1'b1,
-      sbaccess: 3,
+      sbaccess: $clog2(riscv::XLEN / 8),
       default: '0
   };
 
@@ -277,7 +270,7 @@ module vip_chimera_soc
       // Write address as 64-bit double
       jtag_write(dm::SBAddress1, sec_addr[63:32]);
       jtag_write(dm::SBAddress0, sec_addr[31:0]);
-      for (longint i = 0; i <= sec_len; i += 8) begin
+      for (longint i = 0; i <= sec_len; i += riscv::XLEN / 8) begin
         bit checkpoint = (i != 0 && i % 512 == 0);
         if (checkpoint)
           $display(
@@ -286,7 +279,7 @@ module vip_chimera_soc
               sec_len,
               i * 100 / (sec_len > 1 ? sec_len - 1 : 1)
           );
-        jtag_write(dm::SBData1, {bf[i+7], bf[i+6], bf[i+5], bf[i+4]});
+        jtag_write(dm::SBData1, {bf[i+7], bf[i+6], bf[i+5], bf[i+4]}, 0, 1);
         jtag_write(dm::SBData0, {bf[i+3], bf[i+2], bf[i+1], bf[i]}, 1, 1);
       end
     end
@@ -312,7 +305,11 @@ module vip_chimera_soc
     // Repoint execution
     jtag_write(dm::Data1, entry[63:32]);
     jtag_write(dm::Data0, entry[31:0]);
-    jtag_write(dm::Command, 32'h0023_07b1, 0, 1);
+    if (riscv::XLEN == 64) begin
+      jtag_write(dm::Command, 32'h0033_07b1, 0, 1);
+    end else begin
+      jtag_write(dm::Command, 32'h0023_07b1, 0, 1);
+    end
     // Resume hart 0
     jtag_write(dm::DMControl, dm::dmcontrol_t'{resumereq: 1, dmactive: 1, default: '0});
     $display("[JTAG] Resumed hart 0 from 0x%h", entry);
@@ -556,322 +553,6 @@ module vip_chimera_soc
     // We load an image into chip 0 only if it exists
     if (image != "") $readmemh(image, i_spi_norflash.Mem);
   endtask
-
-  ///////////////////
-  //  Serial Link  //
-  ///////////////////
-
-  axi_mst_req_t slink_axi_mst_req, slink_axi_slv_req;
-  axi_mst_rsp_t slink_axi_mst_rsp, slink_axi_slv_rsp;
-
-  AXI_BUS_DV #(
-    .AXI_ADDR_WIDTH(DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH(DutCfg.AxiDataWidth),
-    .AXI_ID_WIDTH  (DutCfg.AxiMstIdWidth),
-    .AXI_USER_WIDTH(DutCfg.AxiUserWidth)
-  ) slink_mst_vip_dv (
-    .clk_i(soc_clk)
-  );
-
-  AXI_BUS #(
-    .AXI_ADDR_WIDTH(DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH(DutCfg.AxiDataWidth),
-    .AXI_ID_WIDTH  (DutCfg.AxiMstIdWidth),
-    .AXI_USER_WIDTH(DutCfg.AxiUserWidth)
-  )
-      slink_mst_ext (), slink_mst_vip (), slink_mst ();
-
-  AXI_BUS #(
-    .AXI_ADDR_WIDTH(DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH(DutCfg.AxiDataWidth),
-    .AXI_ID_WIDTH  (DutCfg.AxiMstIdWidth + 1),
-    .AXI_USER_WIDTH(DutCfg.AxiUserWidth)
-  ) slink_mst_mux ();
-
-  AXI_BUS_DV #(
-    .AXI_ADDR_WIDTH(DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH(DutCfg.AxiDataWidth),
-    .AXI_ID_WIDTH  (DutCfg.AxiMstIdWidth),
-    .AXI_USER_WIDTH(DutCfg.AxiUserWidth)
-  ) slink_slv (
-    .clk_i(soc_clk)
-  );
-
-  // Multiplex internal and external AXI requests
-  axi_mux_intf #(
-    .SLV_AXI_ID_WIDTH(DutCfg.AxiMstIdWidth),
-    .MST_AXI_ID_WIDTH(DutCfg.AxiMstIdWidth + 1),
-    .AXI_ADDR_WIDTH  (DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH  (DutCfg.AxiDataWidth),
-    .AXI_USER_WIDTH  (DutCfg.AxiUserWidth),
-    .NO_SLV_PORTS    (2)
-  ) i_axi_mux_slink (
-    .clk_i (soc_clk),
-    .rst_ni(rst_n),
-    .test_i(test_mode),
-    .slv   ('{slink_mst_vip, slink_mst_ext}),
-    .mst   (slink_mst_mux)
-  );
-
-  // Serialize away added AXI index bits
-  axi_id_serialize_intf #(
-    .AXI_SLV_PORT_ID_WIDTH       (DutCfg.AxiMstIdWidth + 1),
-    .AXI_SLV_PORT_MAX_TXNS       (SlinkMaxTxns),
-    .AXI_MST_PORT_ID_WIDTH       (DutCfg.AxiMstIdWidth),
-    .AXI_MST_PORT_MAX_UNIQ_IDS   (2 ** DutCfg.AxiMstIdWidth),
-    .AXI_MST_PORT_MAX_TXNS_PER_ID(SlinkMaxTxnsPerId),
-    .AXI_ADDR_WIDTH              (DutCfg.AddrWidth),
-    .AXI_DATA_WIDTH              (DutCfg.AxiDataWidth),
-    .AXI_USER_WIDTH              (DutCfg.AxiUserWidth)
-  ) i_axi_id_serialize_slink (
-    .clk_i (soc_clk),
-    .rst_ni(rst_n),
-    .slv   (slink_mst_mux),
-    .mst   (slink_mst)
-  );
-
-  `AXI_ASSIGN(slink_mst_vip, slink_mst_vip_dv)
-
-  `AXI_ASSIGN_FROM_REQ(slink_mst_ext, axi_slink_mst_req)
-  `AXI_ASSIGN_TO_RESP(axi_slink_mst_rsp, slink_mst_ext)
-
-  `AXI_ASSIGN_TO_REQ(slink_axi_mst_req, slink_mst)
-  `AXI_ASSIGN_FROM_RESP(slink_mst, slink_axi_mst_rsp)
-
-  `AXI_ASSIGN_FROM_REQ(slink_slv, slink_axi_slv_req)
-  `AXI_ASSIGN_TO_RESP(slink_axi_slv_rsp, slink_slv)
-
-  // Mirror instance of serial link, reflecting another chip
-  serial_link #(
-    .axi_req_t  (axi_mst_req_t),
-    .axi_rsp_t  (axi_mst_rsp_t),
-    .cfg_req_t  (reg_req_t),
-    .cfg_rsp_t  (reg_rsp_t),
-    .aw_chan_t  (axi_mst_aw_chan_t),
-    .ar_chan_t  (axi_mst_ar_chan_t),
-    .r_chan_t   (axi_mst_r_chan_t),
-    .w_chan_t   (axi_mst_w_chan_t),
-    .b_chan_t   (axi_mst_b_chan_t),
-    .hw2reg_t   (serial_link_single_channel_reg_pkg::serial_link_single_channel_hw2reg_t),
-    .reg2hw_t   (serial_link_single_channel_reg_pkg::serial_link_single_channel_reg2hw_t),
-    .NumChannels(SlinkNumChan),
-    .NumLanes   (SlinkNumLanes),
-    .MaxClkDiv  (SlinkMaxClkDiv)
-  ) i_serial_link (
-    .clk_i        (soc_clk),
-    .rst_ni       (rst_n),
-    .clk_sl_i     (clk),
-    .rst_sl_ni    (rst_n),
-    .clk_reg_i    (clk),
-    .rst_reg_ni   (rst_n),
-    .testmode_i   (test_mode),
-    .axi_in_req_i (slink_axi_mst_req),
-    .axi_in_rsp_o (slink_axi_mst_rsp),
-    .axi_out_req_o(slink_axi_slv_req),
-    .axi_out_rsp_i(slink_axi_slv_rsp),
-    .cfg_req_i    ('0),
-    .cfg_rsp_o    (),
-    .ddr_rcv_clk_i(slink_rcv_clk_o),
-    .ddr_rcv_clk_o(slink_rcv_clk_i),
-    .ddr_i        (slink_o),
-    .ddr_o        (slink_i),
-    .isolated_i   ('0),
-    .isolate_o    (),
-    .clk_ena_o    (),
-    .reset_no     ()
-  );
-
-  // We terminate the slave interface with a random agent
-  axi_test::axi_rand_slave #(
-    .AW                  (DutCfg.AddrWidth),
-    .DW                  (DutCfg.AxiDataWidth),
-    .IW                  (DutCfg.AxiMstIdWidth),
-    .UW                  (DutCfg.AxiUserWidth),
-    .MAPPED              (1'b1),
-    .TA                  (ClkPeriodSys * TAppl),
-    .TT                  (ClkPeriodSys * TTest),
-    .RAND_RESP           (0),
-    .AX_MIN_WAIT_CYCLES  (0),
-    .AX_MAX_WAIT_CYCLES  (SlinkMaxWaitAx),
-    .R_MIN_WAIT_CYCLES   (0),
-    .R_MAX_WAIT_CYCLES   (SlinkMaxWaitR),
-    .RESP_MIN_WAIT_CYCLES(0),
-    .RESP_MAX_WAIT_CYCLES(SlinkMaxWaitResp)
-  ) i_slink_rand_slv = new(
-      slink_slv
-  );
-
-  initial begin
-    i_slink_rand_slv.run();
-  end
-
-  // We use an AXI driver to inject serial link transfers
-  typedef axi_test::axi_driver#(
-    .AW(DutCfg.AddrWidth),
-    .DW(DutCfg.AxiDataWidth),
-    .IW(DutCfg.AxiMstIdWidth),
-    .UW(DutCfg.AxiUserWidth),
-    .TA(ClkPeriodSys * TAppl),
-    .TT(ClkPeriodSys * TTest)
-  ) slink_axi_driver_t;
-
-  slink_axi_driver_t slink_axi_driver = new(slink_mst_vip_dv);
-
-  initial begin
-    @(negedge rst_n);
-    slink_axi_driver.reset_master();
-  end
-
-  task automatic slink_write_beats(input addr_t addr, input axi_pkg::size_t size,
-                                   ref axi_data_t beats[$]);
-    slink_axi_driver_t::ax_beat_t ax = new();
-    slink_axi_driver_t::w_beat_t  w = new();
-    slink_axi_driver_t::b_beat_t  b;
-    int                           i = 0;
-    int                           size_bytes = (1 << size);
-    if (beats.size() == 0) $fatal(1, "[SLINK] Zero-length write requested!");
-    @(posedge clk);
-    if (SlinkAxiDebug) $display("[SLINK] Write to address: %h, len: %0d", addr, beats.size() - 1);
-    ax.ax_addr  = addr;
-    ax.ax_id    = '0;
-    ax.ax_len   = beats.size() - 1;
-    ax.ax_size  = size;
-    ax.ax_burst = axi_pkg::BURST_INCR;
-    if (SlinkAxiDebug) $display("[SLINK] - Sending AW ");
-    slink_axi_driver.send_aw(ax);
-    do begin
-      w.w_strb = i == 0 ? (~('1 << size_bytes)) << addr[AxiStrbBits-1:0] : '1;
-      w.w_data = beats[i];
-      w.w_last = (i == ax.ax_len);
-      if (SlinkAxiDebug) $display("[SLINK] - Sending W (%0d)", i);
-      slink_axi_driver.send_w(w);
-      addr += size_bytes;
-      addr &= size_bytes - 1;
-      i++;
-    end while (i <= ax.ax_len);
-    if (SlinkAxiDebug) $display("[SLINK] - Receiving B");
-    slink_axi_driver.recv_b(b);
-    if (b.b_resp != axi_pkg::RESP_OKAY) $error("[SLINK] - Write error response: %d!", b.b_resp);
-    if (SlinkAxiDebug) $display("[SLINK] - Done");
-  endtask
-
-  task automatic slink_read_beats(input addr_t addr, input axi_pkg::size_t size,
-                                  input axi_pkg::len_t len, ref axi_data_t beats[$]);
-    slink_axi_driver_t::ax_beat_t ax = new();
-    slink_axi_driver_t::r_beat_t  r;
-    int                           i = 0;
-    @(posedge clk)
-      if (SlinkAxiDebug)
-        $display("[SLINK] Read from address: %h, len: %0d", addr, len);
-    ax.ax_addr  = addr;
-    ax.ax_id    = '0;
-    ax.ax_len   = len;
-    ax.ax_size  = size;
-    ax.ax_burst = axi_pkg::BURST_INCR;
-    if (SlinkAxiDebug) $display("[SLINK] - Sending AR");
-    slink_axi_driver.send_ar(ax);
-    do begin
-      if (SlinkAxiDebug) $display("[SLINK] - Receiving R (%0d)", i);
-      slink_axi_driver.recv_r(r);
-      beats.push_back(r.r_data);
-      addr += (1 << size);
-      addr &= (1 << size) - 1;
-      i++;
-      if (r.r_resp != axi_pkg::RESP_OKAY) $error("[SLINK] - Read error response: %d!", r.r_resp);
-    end while (!r.r_last);
-    if (SlinkAxiDebug) $display("[SLINK] - Done");
-  endtask
-
-  task automatic slink_write_32(input addr_t addr, input word_bt data);
-    axi_data_t beats[$];
-    beats.push_back(data << (8 * addr[AxiStrbBits-1:0]));
-    slink_write_beats(addr, 2, beats);
-  endtask
-
-  task automatic slink_poll_bit0(input doub_bt addr, output word_bt data,
-                                 input int unsigned idle_cycles);
-    do begin
-      axi_data_t beats[$];
-      #(ClkPeriodSys * idle_cycles);
-      slink_read_beats(addr, 2, 0, beats);
-      data = beats[0] >> addr[AxiStrbBits-1:0];
-    end while (~data[0]);
-  endtask
-
-  // Load a binary
-  task automatic slink_elf_preload(input string binary, output doub_bt entry);
-    longint sec_addr, sec_len, bus_offset, write_addr;
-    $display("[SLINK] Preloading ELF binary: %s", binary);
-    if (read_elf(binary)) $fatal(1, "[SLINK] Failed to load ELF!");
-    while (get_section(
-        sec_addr, sec_len
-    )) begin
-      byte bf[] = new[sec_len];
-      $display("[SLINK] Preloading section at 0x%h (%0d bytes)", sec_addr, sec_len);
-      if (read_section(sec_addr, bf, sec_len)) $fatal(1, "[SLINK] Failed to read ELF section!");
-      // Write section as fixed-size bursts
-      bus_offset = sec_addr[AxiStrbBits-1:0];
-      for (longint i = 0; i <= sec_len; i += SlinkBurstBytes) begin
-        axi_data_t beats[$];
-        if (i != 0)
-          $display(
-              "[SLINK] - %0d/%0d bytes (%0d%%)",
-              i,
-              sec_len,
-              i * 100 / (sec_len > 1 ? sec_len - 1 : 1)
-          );
-        // Assemble beats for current burst from section buffer
-        for (int b = 0; b < SlinkBurstBytes; b += AxiStrbWidth) begin
-          axi_data_t beat;
-          // We handle incomplete bursts
-          if (i + b - bus_offset >= sec_len) break;
-          for (int e = 0; e < AxiStrbWidth; ++e)
-          if (i + b + e < bus_offset) begin
-            beat[8*e+:8] = '0;
-          end else if (i + b + e - bus_offset >= sec_len) begin
-            beat[8*e+:8] = '0;
-          end else begin
-            beat[8*e+:8] = bf[i+b+e-bus_offset];
-          end
-
-          beats.push_back(beat);
-        end
-        write_addr = sec_addr + (i == 0 ? 0 : i - sec_addr % AxiStrbWidth);
-        // Write this burst
-        slink_write_beats(write_addr, AxiStrbBits, beats);
-      end
-    end
-    void'(get_entry(entry));
-    $display("[SLINK] Preload complete");
-  endtask
-
-  // Run a binary
-  task automatic slink_elf_run(input string binary);
-    doub_bt entry;
-    // Wait for bootrom to ungate Serial Link
-    if (DutCfg.LlcNotBypass) begin
-      word_bt regval;
-      $display("[SLINK] Wait for LLC configuration");
-      slink_poll_bit0(AmLlc + axi_llc_reg_pkg::AXI_LLC_CFG_SPM_LOW_OFFSET, regval, 20);
-    end
-    // Preload
-    slink_elf_preload(binary, entry);
-    // Write entry point
-    slink_write_32(AmRegs + cheshire_reg_pkg::CHESHIRE_SCRATCH_1_OFFSET, entry[63:32]);
-    slink_write_32(AmRegs + cheshire_reg_pkg::CHESHIRE_SCRATCH_0_OFFSET, entry[32:0]);
-    // Resume hart 0
-    slink_write_32(AmRegs + cheshire_reg_pkg::CHESHIRE_SCRATCH_2_OFFSET, 2);
-    $display("[SLINK] Wrote launch signal and entry point 0x%h", entry);
-  endtask
-
-  // Wait for termination signal and get return code
-  task automatic slink_wait_for_eoc(output word_bt exit_code);
-    slink_poll_bit0(AmRegs + cheshire_reg_pkg::CHESHIRE_SCRATCH_2_OFFSET, exit_code, 800);
-    exit_code >>= 1;
-    if (exit_code) $error("[SLINK] FAILED: return code %0d", exit_code);
-    else $display("[SLINK] SUCCESS");
-  endtask
-
 
 endmodule
 
