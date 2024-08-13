9cb455caee4441ad0625a2b0c7c36ebc62ae2f07
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2024-08-13 14:24:23 +0200

    Add comment about `__deploy__` tag

--- chimera.mk
+++ chimera.mk
@@ -13,6 +13,7 @@ update_plic: $(CHS_ROOT)/hw/rv_plic.cfg.hjson
 	sed -i 's/src: .*/src: $(PLIC_NUM_INTRS),/' $<
 	sed -i 's/target: .*/target: $(PLICCORES),/' $<
 
+# SCHEREMO: Technically, there exists a __deploy__* tag for the idma with fixes, but we're checking out the base version
 gen_idma_hw:
 	make -C $(IDMA_ROOT) idma_hw_all
 
