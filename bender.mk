62db2689c20d14958286d287560f446511b4e1df
Sergio Mazzola <smazzola@iis.ee.ethz.ch>
2024-09-10 17:27:24 +0200

    [WIP] include: Add necessary PULP Cluster includes

--- bender.mk
+++ bender.mk
@@ -6,6 +6,9 @@
 # Lorenzo Leone <lleone@iis.ee.ethz.ch>
 
 COMMON_TARGS ?=
-COMMON_TARGS += -t snitch_cluster -t cv32a6_convolve -t cva6 -t rtl
+#COMMON_TARGS += -t snitch_cluster -t cv32a6_convolve -t cva6 -t rtl
+COMMON_TARGS += -t pulp_cluster -t cv32a6_convolve -t cva6 -t rtl
 
 SIM_TARGS = -t test -t sim
+
+VLOG_ARGS += \"+incdir+$(CHIM_ROOT)/hw/include\"
