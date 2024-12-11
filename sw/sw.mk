a5b54da8b6dcb3d8075b3596b645dd8ea90256c9
Lorenzo Leone <lleone@iis.ee.ethz.ch>
2024-12-11 15:49:25 +0100

    Enable relaxation fo rbootrom generation

--- sw/sw.mk
+++ sw/sw.mk
@@ -15,7 +15,7 @@ CHS_SW_INCLUDES += -I$(CHIM_SW_DIR)/include
 # SCHEREMO: use im for platform-level SW, as the smallest common denominator between CVA6 and the Snitch cluster.
 # CVA6's bootrom however needs imc, so override that for this specific case.
 CHS_SW_FLAGS += -falign-functions=64 -march=rv32im -mno-relax
-CHS_BROM_FLAGS += -march=rv32imc
+CHS_BROM_FLAGS += -march=rv32imc -mrelax
 
 CHS_SW_LDFLAGS += -L$(CHIM_SW_DIR)/lib -mno-relax
 
