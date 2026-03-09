0908d697d9d1e93a774105e8303a4a357d867dd5
Philip Wiese <wiesep@iis.ee.ethz.ch>
2026-03-09 15:46:08 +0100

    Fix Snitch Cache Configuration, Update Memory Map, and Improve Simulation Support (#82)
    * Add waves and run script
    
    * Fix address configurations
    
    * Add APB support for fast printf
    
    * Fix Hyperbus Start Region
    
    * Revert Hyperbus Timing Changes for now.
    
    ---------
    
    Co-authored-by: Lorenzo Leone <lleone@iis.ee.ethz.ch>

--- hw/chimera_pkg.sv
+++ hw/chimera_pkg.sv
@@ -126,24 +126,27 @@ ExtClusters
 
   // Memory Island
   localparam byte_bt MemIslandIdx = ClusterIdx[ExtClusters-1] + 1;
+  // WIESEP: Address space 512 KiB
+  localparam doub_bt MemIslRegionLength = 64'h8_0000;
   localparam doub_bt MemIslRegionStart = 64'h4800_0000;
-  localparam doub_bt MemIslRegionEnd = 64'h4804_0000;
+  localparam doub_bt MemIslRegionEnd = MemIslRegionStart + MemIslRegionLength;
 
   // Size of memory island: MemIslNumWideBanks * MemIslNarrowToWideFactor * MemIslWordsPerBank * <BytesPerWord>
   // with BytesPerWord = cfg.AxiDataWidth / 8
   localparam aw_bt MemIslAxiMstIdWidth = 1;
-  localparam byte_bt MemIslNarrowToWideFactor = 16;
+  localparam byte_bt MemIslNarrowToWideFactor = 16;  // 32 bit (narrow) vs. 512 bit (wide)
   localparam byte_bt MemIslNarrowPorts = 1;
   localparam byte_bt MemIslWidePorts = $countones(ChimeraClusterCfg.hasWideMasterPort);
   localparam byte_bt MemIslNumWideBanks = 2;
   localparam shrt_bt MemIslWordsPerBank = 1024;
-  // Memory Island size = 16 * 2 * 1024 * 4 B = 128 KB
+  // WIESEP: Memory Island size = 16 * 2 * 4096 * 32 bit = 512 KB
 
   // Hyperbus
   localparam byte_bt HyperbusIdx = MemIslandIdx + 1;
-  localparam doub_bt HyperbusRegionStart = 64'h5000_0000;
-  //TODO(smazzola): Correct size of HyperRAM?
-  localparam doub_bt HyperbusRegionEnd = HyperbusRegionStart + 64'h1000_0000;
+  // WIESEP: Address space 256 MiB
+  localparam doub_bt HyperbusRegionLength = 64'h1000_0000;
+  localparam doub_bt HyperbusRegionStart = 64'h8000_0000;
+  localparam doub_bt HyperbusRegionEnd = HyperbusRegionStart + HyperbusRegionLength;
 
   localparam int unsigned HypNumPhys = 1;
   localparam int unsigned HypNumChips = 2;
