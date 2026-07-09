c728ebea2250a16586b273a2760049a9f0c6d562
Philip Wiese <wiesep@iis.ee.ethz.ch>
2026-07-09 17:27:29 +0200

    Cleanup and update chimera-sdk

--- target/sim/vsim/setup.chimera_soc.tcl
+++
@@ -1,8 +0,0 @@
-# Copyright 2024 ETH Zurich and University of Bologna.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Moritz Scherer <scheremo@iis.ee.ethz.ch>
-
-set BINARY ../../../sw/tests/testCluster.memisl.elf
-set SELCFG 0
